e5b14d8b32cda059e5e80cbc7f51a4e4da901849
vipul43 <saifunny43@gmail.com>
2022-01-13 01:26:49 +0530

    new views

---
+++ frontend-blip/src/views/Dashboard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="dashboard">
+    This is {{ authenticated ? username : "Unauthorized" }} Dashboard.
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "Dashboard",
+  computed: {
+    ...mapGetters({
+      authenticated: "auth/authenticated",
+      username: "auth/username",
+    }),
+  },
+  metaInfo() {
+    return {
+      title: "Dashboard - Project BLiP",
+    };
+  },
+};
+</script>
---
+++ frontend-blip/src/views/Settings.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="settings">This is Settings.</div>
+</template>
+
+<script>
+export default {
+  name: "Settings",
+  metaInfo() {
+    return {
+      title: "Account Settings - Project BLiP",
+    };
+  },
+};
+</script>
