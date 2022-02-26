65ca1534651f7c5a51a38b4040ee68909d1bb24b
vipul43 <saifunny43@gmail.com>
2022-02-26 20:14:18 +0530

    updated faqs view with search

--- frontend-blip/src/views/FAQ.vue
+++ frontend-blip/src/views/FAQ.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="faq">
     <v-container>
+      <v-text-field
+        v-model="questionSearch"
+        label="Search your question"
+        outlined
+        clearable
+      ></v-text-field>
       <v-row justify="center">
         <v-col>
           <v-expansion-panels popout>
-            <v-expansion-panel v-for="(faq, i) in faqs" :key="i">
+            <v-expansion-panel v-for="(faq, i) in displayFaqs" :key="i">
               <v-expansion-panel-header disable-icon-rotate>
                 <template v-slot:actions>
                   <v-icon> mdi-lightbulb-on </v-icon>
@@ -29,8 +35,25 @@ import { getAllFAQs } from "../api.js";
 export default {
   name: "FAQ",
   data: () => ({
+    questionSearch: null,
     faqs: [],
   }),
+  computed: {
+    displayFaqs() {
+      if (this.questionSearch) {
+        return this.faqs.filter((faq) => {
+          return (
+            faq.question
+              .toLowerCase()
+              .includes(this.questionSearch.toLowerCase()) ||
+            faq.answer.toLowerCase().includes(this.questionSearch.toLowerCase())
+          );
+        });
+      } else {
+        return this.faqs;
+      }
+    },
+  },
   async mounted() {
     getAllFAQs()
       .then((response) => {
